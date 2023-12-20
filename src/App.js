649ebfb6b2b6b6e5f90df8bbde20d4f7b59d1046
bagheladarsh007 <bagheladarsh97@gmail.com>
2023-12-20 20:14:02 +0530

    added files

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import Header from "./Components/Header/Header";
+import Hero from "./Components/Hero/Hero";
+import About from "./Components/About/About";
+import Footer from "./Components/Footer/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Hero />
+      <About />
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
